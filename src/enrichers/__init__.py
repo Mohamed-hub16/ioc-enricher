12295600a7840facb13fd16a62450fbc58a14994
Mohamed <mesrimohamed16@gmail.com>
2026-05-11 21:32:41 +0200

    fix: remove MXToolbox (paid API only), Shodan domains-only restriction

--- src/enrichers/__init__.py
+++ src/enrichers/__init__.py
@@ -3,10 +3,10 @@ Enricher registry.
 Each function signature: enrich(ioc: IOC) -> EnrichmentResult
 """
 
-from src.enrichers import abuseipdb, virustotal, urlscan, shodan, mxtoolbox
+from src.enrichers import abuseipdb, virustotal, urlscan, shodan
 
 ENRICHERS_BY_TYPE: dict[str, list] = {
     "ip":     [abuseipdb.enrich, virustotal.enrich, urlscan.enrich, shodan.enrich],
-    "domain": [virustotal.enrich, urlscan.enrich, mxtoolbox.enrich],
+    "domain": [virustotal.enrich, urlscan.enrich],
     "hash":   [virustotal.enrich],
 }
